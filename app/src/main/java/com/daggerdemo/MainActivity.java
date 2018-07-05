41befb634458196f0ca2a939763882696d16479a
Rana Ranvijay Singh <rana.singh@globant.com>
2018-07-05 14:18:20 +0530

    Code without dagger.

--- app/src/main/java/com/daggerdemo/MainActivity.java
+++ app/src/main/java/com/daggerdemo/MainActivity.java
@@ -5,22 +5,17 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import javax.inject.Inject;
-
-import dagger.android.AndroidInjection;
-
 public class MainActivity extends AppCompatActivity implements MainContract.View {
 
     private TextView mTextViewName;
-    @Inject
-    MainContract.Presenter mPresenter;
+    private MainContract.Presenter mPresenter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextViewName = findViewById(R.id.textViewName);
+        mPresenter = new MainPresenterImp(this, new DummyData());
         mPresenter.presentUserName();
     }
 
--- app/src/main/java/com/daggerdemo/MainPresenterImp.java
+++ app/src/main/java/com/daggerdemo/MainPresenterImp.java
@@ -2,16 +2,16 @@ package com.daggerdemo;
 
 public class MainPresenterImp implements MainContract.Presenter {
     private final MainContract.View mView;
-    private final DummyData data;
+    private final DummyData mData;
 
     public MainPresenterImp(MainContract.View view, DummyData data) {
         mView = view;
-        this.data = data;
+        mData = data;
     }
 
     @Override
     public void presentUserName() {
-        String name = data.getUserName();
+        String name = mData.getUserName();
         mView.setUserName(name);
     }
 }
